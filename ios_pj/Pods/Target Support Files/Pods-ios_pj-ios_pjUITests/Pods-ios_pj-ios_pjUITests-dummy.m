cca650de15ee574d61a69d330f0c619f0ba40475
dong.nguyen <luongdong059@gmail.com>
2022-07-30 11:44:00 +0700

    add ios pj

---
+++ ios_pj/Pods/Target Support Files/Pods-ios_pj-ios_pjUITests/Pods-ios_pj-ios_pjUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_pj_ios_pjUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_pj_ios_pjUITests
+@end
---
+++ ios_pj/ios_pjUITests/ios_pjUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_pjUITestsLaunchTests.swift
+//  ios_pjUITests
+//
+//  Created by MAC MINI  on 7/30/22.
+//
+
+import XCTest
+
+class ios_pjUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
